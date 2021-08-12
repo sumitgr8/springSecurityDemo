4f353d7d190f9d6262597a39179db89396d74fa0
Sumit <sumit11me191@outlook.com>
2021-08-12 23:46:45 +0530

    Added Spring Security Authorizations

--- src/main/java/com/demo/springSecurity/config/AppSecurityConfig.java
+++ src/main/java/com/demo/springSecurity/config/AppSecurityConfig.java
@@ -3,27 +3,45 @@ package com.demo.springSecurity.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	UserDetailsService userDetailsService;
 
+//	@Bean
+//	@Override
+//	protected UserDetailsService userDetailsService() {
+//		// TODO Auto-generated method stub
+//		List<UserDetails> users = new ArrayList<>();
+//		users.add(User.withDefaultPasswordEncoder().username("sumit").roles("USER").password("sumit123").build());
+//		users.add(User.withDefaultPasswordEncoder().username("abc").roles("USER").password("abc123").build());
+//		return new InMemoryUserDetailsManager(users);
+//	}
+	
 	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
-		// TODO Auto-generated method stub
-		List<UserDetails> users = new ArrayList<>();
-		users.add(User.withDefaultPasswordEncoder().username("sumit").roles("USER").password("sumit123").build());
-		users.add(User.withDefaultPasswordEncoder().username("abc").roles("USER").password("abc123").build());
-		return new InMemoryUserDetailsManager(users);
+	public AuthenticationProvider authProvide() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		//provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
+		return provider;
+		
 	}
 
 	
